2939bf1612f2b8ddbfe445eaa55c2603684d4180
Ivan <ivanpetrlikazabache@gmail.com>
2020-07-04 20:13:21 -0500

    INICIO DEL PROYECTO
    INICIO DEL  PROYECTO DE EVALUACION EN  LINEA
    ATENTAMENTE
    DR.IVAN PETRLIK  AZABACHE

---
+++ src/main/java/UTIL/ConexionBD.java
@@ -0,0 +1,22 @@
+package UTIL;
+import  java.sql.*;
+public class ConexionBD 
+{
+    public  static Connection getConexionBD()
+    { 
+        Connection  cn=null;
+        try 
+        {
+          Class.forName("com.mysql.jdbc.Driver");    
+          cn=DriverManager.getConnection("jdbc:mysql://localhost/mydb6","root",""); 
+        System.out.println("Se Conecto !!!!");
+        } catch (Exception e) 
+        {  System.out.println("No Se Conecto !!!!");
+        }    
+    return  cn;
+    }
+  public  static void main(String  args[])
+  {
+      getConexionBD();
+  }
+}
